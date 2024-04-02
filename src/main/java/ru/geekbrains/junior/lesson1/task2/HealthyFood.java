31612d59bde582c854e6db6d30c90609b237a611
Russell <russellfm@mail.ru>
2024-04-02 15:05:40 +0400

    Lesson 1

---
+++ src/main/java/ru/geekbrains/junior/lesson1/task2/HealthyFood.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.junior.lesson1.task2;
+
+/**
+ * Здоровая еда
+ */
+public interface HealthyFood extends Food{
+}
+
---
+++ src/main/java/ru/geekbrains/junior/lesson1/task2/Snack.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.junior.lesson1.task2;
+
+/**
+ * Снек (легкая закуска)
+ */
+public interface Snack extends Food {
+}
+
